e61d3ff29bb5313790bb12c7f49802f7170841dc
IntanAlfaraAudia <intanalfaradia@gmail.com>
2026-03-02 15:49:21 +0800

    final posttest

---
+++ POSTTEST_1/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        From1 = New Label()
+        lblIPS = New Label()
+        txtIPS = New TextBox()
+        btnTambah = New Button()
+        btnReset = New Button()
+        lblIPKText = New Label()
+        txtIPK = New TextBox()
+        lblPredikat = New Label()
+        SuspendLayout()
+        ' 
+        ' From1
+        ' 
+        From1.AutoSize = True
+        From1.Location = New Point(319, 30)
+        From1.Name = "From1"
+        From1.Size = New Size(110, 25)
+        From1.TabIndex = 0
+        From1.Text = "Kalkulator IP"
+        ' 
+        ' lblIPS
+        ' 
+        lblIPS.AutoSize = True
+        lblIPS.Location = New Point(81, 99)
+        lblIPS.Name = "lblIPS"
+        lblIPS.Size = New Size(105, 25)
+        lblIPS.TabIndex = 1
+        lblIPS.Text = "IP Semester"
+        ' 
+        ' txtIPS
+        ' 
+        txtIPS.Location = New Point(245, 99)
+        txtIPS.Name = "txtIPS"
+        txtIPS.Size = New Size(288, 31)
+        txtIPS.TabIndex = 2
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(256, 146)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(112, 34)
+        btnTambah.TabIndex = 3
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(390, 146)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(112, 34)
+        btnReset.TabIndex = 4
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' lblIPKText
+        ' 
+        lblIPKText.AutoSize = True
+        lblIPKText.Location = New Point(84, 248)
+        lblIPKText.Name = "lblIPKText"
+        lblIPKText.Size = New Size(107, 25)
+        lblIPKText.TabIndex = 5
+        lblIPKText.Text = "IP Kumulatif"
+        lblIPKText.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' txtIPK
+        ' 
+        txtIPK.BackColor = SystemColors.Window
+        txtIPK.ForeColor = SystemColors.WindowFrame
+        txtIPK.Location = New Point(245, 242)
+        txtIPK.Name = "txtIPK"
+        txtIPK.ReadOnly = True
+        txtIPK.ScrollBars = ScrollBars.Vertical
+        txtIPK.Size = New Size(294, 31)
+        txtIPK.TabIndex = 6
+        txtIPK.Text = "0.00"
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.AutoSize = True
+        lblPredikat.Location = New Point(341, 316)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(19, 25)
+        lblPredikat.TabIndex = 7
+        lblPredikat.Text = "-" & vbCrLf
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10.0F, 25.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblPredikat)
+        Controls.Add(txtIPK)
+        Controls.Add(lblIPKText)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Controls.Add(txtIPS)
+        Controls.Add(lblIPS)
+        Controls.Add(From1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents From1 As Label
+    Friend WithEvents lblIPS As Label
+    Friend WithEvents txtIPS As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lblIPKText As Label
+    Friend WithEvents txtIPK As TextBox
+    Friend WithEvents lblPredikat As Label
+
+End Class
---
+++ POSTTEST_1/Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+
+Public Class Form1
+
+    Dim listIPS As New List(Of Double)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.StartPosition = FormStartPosition.CenterScreen
+        txtIPK.Text = "0.00"
+        txtIPK.ReadOnly = True
+        lblPredikat.Text = "-"
+    End Sub
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Try
+            Dim ips As Double = Convert.ToDouble(txtIPS.Text)
+
+            If ips < 0 Or ips > 4 Then
+                MessageBox.Show("IP harus antara 0 - 4")
+                Exit Sub
+            End If
+
+            listIPS.Add(ips)
+
+            Dim ipk As Double = listIPS.Average()
+
+            txtIPK.Text = ipk.ToString("0.00")
+
+            If ipk >= 2.0 And ipk <= 2.75 Then
+                lblPredikat.Text = "Cukup"
+            ElseIf ipk >= 2.76 And ipk <= 3.0 Then
+                lblPredikat.Text = "Memuaskan"
+            ElseIf ipk >= 3.01 Then
+                lblPredikat.Text = "Sangat Memuaskan"
+            Else
+                lblPredikat.Text = "-"
+            End If
+
+            txtIPS.Clear()
+            txtIPS.Focus()
+
+        Catch
+            MessageBox.Show("Masukkan angka yang valid!")
+        End Try
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        listIPS.Clear()
+        txtIPK.Text = "0.00"
+        lblPredikat.Text = "-"
+        txtIPS.Clear()
+        txtIPS.Focus()
+    End Sub
+
+End Class
